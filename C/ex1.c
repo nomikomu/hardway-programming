cbd8de017f3dd1fc093c48fdc01c458c6658500d
defm03 <yutsuro@gmail.com>
2013-03-25 20:23:26 +0100

    C programming - Exercise 1: Dust Off That Compiler // stdio & puts, putchar etc.

---
+++ C/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  /* stdio - puts & putchar testing */
+  puts("a \n\n");
+  int c;
+  c = putchar (-22);
+  puts("Hello world.\n");
+  puts("nrml test: oh mai gah");
+  puts("symbol test 2: @#-+");
+  puts("slash test: //\\ abc \n");
+  puts("i share my love: <3");
+  puts("a \n\n");
+  return 0;
+}
+
+/* Exercise 1: Dust Off That Compiler*/
+/* extra credit:
+   dot_one: done
+   dot_two: done
+ dot_three: done */
