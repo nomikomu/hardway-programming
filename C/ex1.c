e331f4fffe5bc88e0a97f6b7d238d854ffb70173
defm03 <yutsuro@gmail.com>
2013-03-26 19:29:58 +0100

    Exercise 2: Make Is Your Python Now; // GNUmake = make mastering; Makefile

--- C/ex1.c
+++ C/ex1.c
@@ -20,3 +20,15 @@ int main(int argc, char *argv[])
    dot_one: done
    dot_two: done
  dot_three: done */
+
+/*Exercise 2: Make Is Your Python Now*/
+/*extra credit:
+   dot_one: done
+   dot_two: done
+   dot_three: no man for cc
+              no info about -Wall, -g
+              in info and --help
+              found on: site from dot_four
+   dot_four: done - from (pl) site.
+             www.programuj.com/artykuly/linux/makefile.php
+   dot_five: done*/
