4ca4c9a15f6c2ed0617449d4f5381278a3c0420b
defm03 <yutsuro@gmail.com>
2013-04-06 08:25:58 +0200

    Concept of Zero - Going into multidimensional math

---
+++ concept_of_zero.py
@@ -0,0 +1,18 @@
+x = 1
+y = 1
+x_list = []
+y_list = []
+def add_group():
+  while x:
+    print x
+    x += 1
+    x_list.append(x)
+def neg_group():
+  while y:
+    print y
+    y -= 1
+    y_list.append(y)
+if True:
+  add_group()
+  neg_group()
+  zero == x_list[0:] - y_list[0:] 
