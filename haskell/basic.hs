d9dc81dff1f0b601e3c20972c9c89a18b5e741fe
defm03 <yutsuro@gmail.com>
2013-05-28 17:40:03 +0200

    Functions and lists. Doing something i gues.

--- haskell/basic.hs
+++ haskell/basic.hs
@@ -6,6 +6,14 @@ doubleSmallNum x = if x > 100
                      then x
                      else x*2
 doubleSmallNum' x = (if x > 100 then x else x*2) + 1
+
 -- definition
 iAndMyBoobz = "I love my boobies."
+
+-- fried chicken
+defMoe x y z = succ x * ((succ x * succ y * succ z) + y*z)
+
+-- list function
+defMoe' x y = (if x > 100 then x:x:[x,y+y] else x:[x]++[y])
+
 
