7e6f6e0e06280924d8bf4493c8b31c3db844fb4a
defm03 <yutsuro@gmail.com>
2013-05-28 17:12:17 +0200

    If, then, else, definition and that sort of stuff

--- haskell/basic.hs
+++ haskell/basic.hs
@@ -1,3 +1,11 @@
 doubleMe x = x + x
-doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+doubleUsOld x y = x*2 + y*2
 doubleUsS x y = (succ x*2) + (succ y*2)
+doubleSmallNum x = if x > 100
+                     then x
+                     else x*2
+doubleSmallNum' x = (if x > 100 then x else x*2) + 1
+-- definition
+iAndMyBoobz = "I love my boobies."
+
