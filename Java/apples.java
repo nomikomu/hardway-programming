a8c72d4ac1fdf90a851168460f2c2cf9c41fe352
defm03 <yutsuro@gmail.com>
2013-06-01 08:35:01 +0200

    Java; hello dza warldo~

---
+++ Java/apples.java
@@ -0,0 +1,6 @@
+class apples{
+  //method
+  public static void main(String args[]){
+    System.out.println("Hello DZA WARLDO!");
+  }
+}
---
+++ Java/dzawarldo.java
@@ -0,0 +1,20 @@
+//char a;
+//a = 'D';
+//int b = 'b'
+
+// im just tryin'
+
+//int dni[] = new int[31]; // table
+//int m_and_d[][] = new int[12][31]; // 2d table
+//long c;
+//c = 0xa9
+//float d;
+//d = 0.5
+//Charset.forName("UTF-8").encode(myString)
+
+package testinout;
+class DzaWarldo {
+  public static void main(String[] args) {
+    System.out.println("HALLO DZA WARLDO!");
+  }
+}
