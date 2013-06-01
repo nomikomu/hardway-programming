c2e09efa7a810e609e3cad7884c0edf95f4b74e6
defm03 <yutsuro@gmail.com>
2013-06-01 11:06:07 +0200

    Java - Switch Statement

--- Java/desu.java
+++ Java/desu.java
@@ -1,14 +1,35 @@
 
 class desu{
   public static void main(String args[]){
-    int boy, girl;
-    boy = 18;
-    girl= 40;
+    int age;
+    age = 15;
 
-    if(boy > 10 || girl < 60){
-      System.out.println("You can enter");
-    }else{
-      System.out.println("You can not enter");
+    switch (age){
+    case 1:
+      System.out.println("you can crawl");
+      break;
+    case 2:
+      System.out.println("you can talk");
+      break;
+    case 18:
+      System.out.println("you can watch porn");
+      break;
+    default:
+      System.out.println("you can do absolutely nothing");
+    break;
     }
+
+    // Logical operator test
+    /*
+      int boy, girl;
+      boy = 18;
+      girl= 40;
+
+      if(boy > 10 || girl < 60){
+        System.out.println("You can enter");
+      }else{
+        System.out.println("You can not enter");
+      }
+    */
   }
 }
