1dbe1ed9a867c32dc82d42c6772be9a8dfff97c3
defm03 <yutsuro@gmail.com>
2013-06-01 08:44:03 +0200

    Java - Variables testing

--- Java/apples.java
+++ Java/apples.java
@@ -1,6 +1,15 @@
 class apples{
   //method
   public static void main(String args[]){
-    System.out.println("Hello DZA WARLDO!");
+    System.out.println("lel");
+    
+    //varible
+    double tuna;
+    tuna = 3.14;
+
+    System.out.print("I want ");
+    System.out.print(tuna);
+    System.out.println(" movies");
+    System.out.print("apples");
   }
 }
