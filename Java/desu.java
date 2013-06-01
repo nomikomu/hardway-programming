ca904e4d7cad985c068b539a879fac5974b2a177
defm03 <yutsuro@gmail.com>
2013-06-01 10:58:03 +0200

    Java - Logical Operators

---
+++ Java/desu.java
@@ -0,0 +1,14 @@
+
+class desu{
+  public static void main(String args[]){
+    int boy, girl;
+    boy = 18;
+    girl= 40;
+
+    if(boy > 10 || girl < 60){
+      System.out.println("You can enter");
+    }else{
+      System.out.println("You can not enter");
+    }
+  }
+}
