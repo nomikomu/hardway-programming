052214d04eb86ec8d6766aee68a0783c1cd486bf
defm03 <yutsuro@gmail.com>
2013-06-01 09:10:39 +0200

    Java - modulus, remainder, int

--- Java/apples.java
+++ Java/apples.java
@@ -8,25 +8,38 @@ class apples{
     Scanner bax = new Scanner(System.in);
 
     //small calc
-    double fnum, snum, anwser;
-    System.out.println("num one: ");
-    fnum = bax.nextDouble();
-    System.out.println("num two: ");
-    snum = bax.nextDouble();
-    anwser = fnum + snum;
-    System.out.println("out: ");
-    System.out.print(anwser);
+    /*
+      double fnum, snum, anwser;
+      System.out.println("num one: ");
+      fnum = bax.nextDouble();
+      System.out.println("num two: ");
+      snum = bax.nextDouble();
+      anwser = fnum + snum;
+      System.out.println("out: ");
+      System.out.print(anwser);
+    */
 
     //varible
-    double tuna;
-    tuna = 3.14;
+    /*
+      double tuna;
+      tuna = 3.14;
+    */
+    
+    //print basic stuff
+    /*
+      System.out.println("I want ");
+      System.out.print(tuna);
+      System.out.println(" movies");
+      System.out.println("apples");
+    */
 
-    //System.out.println("I want ");
-    //System.out.print(tuna);
-    //System.out.println(" movies");
-    //System.out.println("apples");
+    int girls, boys, people;
+    girls = 7;
+    boys = 3;
+    people = girls % boys; //modulus - remainder
+    System.out.println(people);
 
     //print sax in nextLine
-    System.out.println(sax.nextLine());
+    //System.out.println(sax.nextLine());
   }
 }
