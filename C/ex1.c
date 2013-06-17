5a0282e86efa69738eb273254568751b4ff34f24
defm03 <yutsuro@gmail.com>
2013-06-17 19:48:59 +0200

    re-C; I will get into it now

--- C/ex1.c
+++ C/ex1.c
@@ -1,34 +1,8 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-  /* stdio - puts & putchar testing */
-  puts("a \n\n");
-  int c;
-  c = putchar (-22);
-  puts("Hello world.\n");
-  puts("nrml test: oh mai gah");
-  puts("symbol test 2: @#-+");
-  puts("slash test: //\\ abc \n");
-  puts("i share my love: <3");
-  puts("a \n\n");
+int main(int argc, char *argv[]){
+  puts("Hello dza warldo.");
+  puts("I fuck your bitchez.");
+  puts("Its\nfucked up");
   return 0;
 }
 
-/* Exercise 1: Dust Off That Compiler*/
-/* extra credit:
-   dot_one: done
-   dot_two: done
- dot_three: done */
-
-/*Exercise 2: Make Is Your Python Now*/
-/*extra credit:
-   dot_one: done
-   dot_two: done
-   dot_three: no man for cc
-              no info about -Wall, -g
-              in info and --help
-              found on: site from dot_four
-   dot_four: done - from (pl) site.
-             www.programuj.com/artykuly/linux/makefile.php
-   dot_five: done*/
