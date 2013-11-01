643657b87669f438965b8b4188533e7546d6515c
patrick brisbin <pbrisbin@gmail.com>
2013-11-01 19:03:39 +0100

    C -- just some sizeof and arrays

---
+++ C/zed/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int fuckyou = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d fuck you. \n", fuckyou);
+
+    return 0;
+}
