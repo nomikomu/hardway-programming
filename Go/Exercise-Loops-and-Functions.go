b7d1dd5f1399656700907b14f73c8b0da37dd06a
色とりどりの <yutsuro@gmail.com>
2014-05-05 10:50:49 +0200

    Exercise: Slices - Showing Picture

--- Go/Exercise-Loops-and-Functions.go
+++ Go/Exercise-Loops-and-Functions.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
+// Searching Sqrt - Newton's method
 func Sqrt(x float64) float64 {
-    guess := x
-    for i := 0; i < 10; i++ {
-        guess -= (math.Pow(guess,2) - 2) / (2.0*guess)
-    }
-    return guess
+	guess := x
+	for i := 0; i < 10; i++ {
+		guess -= (math.Pow(guess, 2) - 2) / (2.0 * guess)
+	}
+	return guess
 }
 
+// Difference between math.Sqrt and Sqrt
 func SqrtDiff(x, y float64) float64 {
-    diff := float64(math.Sqrt(x)) - y
-    if diff < 0 {
-       diff = -diff
-    }
-    return diff
+	diff := float64(math.Sqrt(x)) - y
+	if diff < 0 {
+		diff = -diff
+	}
+	return diff
 }
 
 func main() {
-    fmt.Println(Sqrt(2))
-    fmt.Println(math.Sqrt(2))
-    fmt.Println(float64(math.Sqrt(2)) - Sqrt(2))
-    fmt.Println(SqrtDiff(2, Sqrt(2)))
+	fmt.Println(Sqrt(2))
+	fmt.Println(math.Sqrt(2))
+	fmt.Println(float64(math.Sqrt(2)) - Sqrt(2))
+	fmt.Println(SqrtDiff(2, Sqrt(2)))
 }
---
+++ Go/Exercise-Slices.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"code.google.com/p/go-tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dx)
+	for i := range a {
+		a[i] = make([]uint8, dy)
+		for j := range a {
+			a[i][j] = uint8((i + j) / 2)
+		}
+	}
+	return a
+}
+
+func main() {
+	pic.Show(Pic)
+}
